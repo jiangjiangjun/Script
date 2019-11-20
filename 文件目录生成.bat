f7be9e4c017afd15e011191fa15d69f52f3969fd
jiangjiangjun <beatlejwlove@gmail.com>
2019-11-20 16:10:24 +0800

    解决md文档在GitHub上换行显示不正常，必须在最后追加最少两个空格在换行。更新文件目录生成.bat-------测试

--- 文件目录生成.bat
+++ 文件目录生成.bat
@@ -1,1 +1,16 @@
+
+del list.txt
+
 tree /f /a >list.txt
+
+@echo off
+set after=    
+for /f "delims=" %%i in (list.txt) do (
+  if "%%i"=="" (
+    echo.>>1.txt
+  ) else (
+    echo %%i%after%>>1.txt
+  )
+)
+del list.txt
+ren 1.txt list.txt
